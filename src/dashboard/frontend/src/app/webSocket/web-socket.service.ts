08aa3d55e9e05910e2983df6e381fb529319215d
safeforce <mittendorfshelby359475@gmail.com>
2026-01-29 19:05:39 +0700

    ui cache

--- src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
+++ src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
@@ -71,7 +71,7 @@ export class WebSocketService {
 
   constructor() {
     this.webSocket = new Socket({
-      url: "http://192.168.0.91:5005",
+      url: "http://192.168.50.1:5005",
       options: {},
     });
 
