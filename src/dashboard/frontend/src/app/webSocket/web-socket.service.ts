d658fa2960c71341f79fce2e615559f274b85657
safeforce <mittendorfshelby359475@gmail.com>
2026-01-29 18:22:26 +0700

    Merge Perception with UI

--- src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
+++ src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
@@ -71,7 +71,7 @@ export class WebSocketService {
 
   constructor() {
     this.webSocket = new Socket({
-      url: "http://192.168.1.24:5005",
+      url: "http://192.168.50.1:5005",
       options: {},
     });
 
